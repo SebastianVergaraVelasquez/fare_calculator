12e8b55466f81969f6ba892a8017dfef4fce7838
“sebastianvergaravelasquez” <sv.velasquez36@gmail.com>
2026-04-13 16:02:33 -0500

    add JSON serialization and configure application module

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,24 @@
 package org.sebastianv
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
+
 fun Application.module() {
-    configureRouting()
+    val routeRepository = RouteRepositoryJson()
+    val fareProcessingService = FareProcessingService(routeRepository)
+
+    configureSerialization()
+    configureRouting(fareProcessingService)
 }
